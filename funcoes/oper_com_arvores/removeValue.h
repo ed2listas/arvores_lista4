7458a0d1882396e4da43d08872d5a9dc0cf856e8
Joberth Rogers <joberth.rogers18@gmail.com>
2018-09-24 01:16:42 -0300

    adding methods of remove

--- funcoes/oper_com_arvores/removeValue.h
+++ funcoes/oper_com_arvores/removeValue.h
@@ -35,7 +35,7 @@ tree * removeValue(tree * arvore, int valor, int tipoDeArvore) {
     free(aux);
   }
 
-  // caso o No tiver filho a esquerda
+  // caso o No tiver filho a esq
   if (aux->left != NULL && aux->right == NULL) {
     if (ant->value > valor)
       ant->left = aux->left;
@@ -43,7 +43,7 @@ tree * removeValue(tree * arvore, int valor, int tipoDeArvore) {
       ant->right = aux->left;
     free(aux);
   }
-  // caso o No tiver filho a direita
+  // caso o No tiver filho a dir
   if (aux->left == NULL && aux->right != NULL) {
     if (ant->value > valor)
       ant->left = aux->right;
@@ -51,17 +51,6 @@ tree * removeValue(tree * arvore, int valor, int tipoDeArvore) {
       ant->right = aux->right;
     free(aux);
   }
-  //caso o No tiver 2 filhos
-  // if (aux->left != NULL && aux->right != NULL) {
-  //   folha = aux->right;
-  //   while (folha->left == NULL || folha->right == NULL) {
-  //     folha = aux->left;
-  //   }
-  //   aux->value = folha->value;
-  //   arvore = removeValue(arvore, folha->value);
-  //   printf("removido do meio\n");
-  //   pausar();
-  // }
   if (aux->left != NULL && aux->right != NULL) {
     folha = aux->right;
     antFolha = aux;
@@ -72,8 +61,111 @@ tree * removeValue(tree * arvore, int valor, int tipoDeArvore) {
     temp = folha->value;
     arvore = removeValue(arvore, folha->value, tipoDeArvore);
     aux->value = temp;
-    //printf("removido do meio\n");
-    //pausar();
   }
   return arvore;
+}
+
+arvore_avl* men_dir(arvore_avl *arvore){
+    while(arvore->esq){
+        arvore = arvore->esq;
+    }
+    return arvore;
+}
+
+void remover_avl(arvore_avl *raiz, arvore_avl *aux){
+    arvore_avl *aux2, *salva = aux->pai;
+    if(aux != raiz){
+        if(!aux->esq && !aux->dir){
+            if(aux->valor < aux->pai->valor){
+                aux->pai->esq = NULL;
+            }
+            else{
+                aux->pai->dir = NULL;
+            }
+            free(aux);
+        }
+        else if(!aux->dir){
+            if(aux->valor < aux->pai->valor){
+                aux->pai->esq = aux->esq;
+            }
+            else{
+                aux->pai->dir = aux->esq;
+            }
+            aux->esq->pai = aux->pai;
+            free(aux);
+        }
+        else if(!aux->esq){
+            if(aux->valor < aux->pai->valor){
+                aux->pai->esq = aux->dir;
+            }
+            else{
+                aux->pai->dir = aux->dir;
+            }
+            aux->dir->pai = aux->pai;
+            free(aux);
+        }
+        else{
+            aux2 = men_dir(aux->dir);
+            aux->valor = aux2->valor;
+
+            remover_avl(raiz, aux2);
+        }
+    }
+    else{
+        if(!aux->esq && !aux->dir){
+            free(aux);
+            raiz = NULL;
+        }
+        else if(!aux->dir){
+            aux->esq->pai = NULL;
+            raiz = aux->esq;
+            free(aux);
+        }
+        else if(!aux->esq){
+            aux->dir->pai = NULL;
+            raiz = aux->dir;
+            free(aux);
+        }
+        else{
+            aux2 = men_dir(aux->dir);
+            aux->valor = aux2->valor;
+
+            remover_avl(raiz,aux2);
+        }
+    }
+    if(salva){
+        while(salva){
+            salva->fator_bal = altura(salva->dir) - altura(salva->esq);
+
+            if(salva->fator_bal > 1 || salva->fator_bal < -1){
+                salva = balancear(raiz, salva);
+            }
+
+            salva = salva->pai;
+        }
+    }
+}
+
+void remove_chamada(arvore_avl *raiz, int valor){
+    arvore_avl *aux;
+    arvore_avl *aux2;
+
+    aux2 = raiz;
+
+    while(aux2 && aux2->valor != valor){
+        if(valor < aux->valor){
+            aux2 = aux2->esq;
+        }
+        else{
+            aux2 = aux2->dir;
+        }
+    }
+
+    if(aux){
+        remover_avl(raiz,aux);
+    }
+
+    else{
+        printf("Nao existe!\n");
+    }
 }
